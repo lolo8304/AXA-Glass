b6a2a5ca1f26e23fb1044936d74d23b8882ccf72
Nicolas Lauquin <nicolas@atelierdumobile.com>
2014-09-15 18:35:01 +0200

    New workflow : axa home + My consultant

---
+++ Axa Glass/MyConsultantViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyConsultantViewController.h
+//  Axa Glass
+//
+//  Created by Nicolas Lauquin on 15/09/2014.
+//  Copyright (c) 2014 Axa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyConsultantViewController : UIViewController
+- (IBAction)close:(id)sender;
+
+@end
---
+++ Axa Glass/MyConsultantViewController.m
@@ -0,0 +1,17 @@
+//
+//  MyConsultantViewController.m
+//  Axa Glass
+//
+//  Created by Nicolas Lauquin on 15/09/2014.
+//  Copyright (c) 2014 Axa. All rights reserved.
+//
+
+#import "MyConsultantViewController.h"
+
+@implementation MyConsultantViewController
+
+- (IBAction)close:(id)sender {
+	[self dismissViewControllerAnimated:YES completion:Nil];
+}
+
+@end
