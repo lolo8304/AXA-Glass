4266e966374b450bb540961f53b1873cfc268ae9
Lorenz Hänggi <lolo8304@gmail.com>
2014-10-01 23:59:41 +0200

    add new details protection sceen

--- Axa Glass/MyProtectionDetailsViewController.h
+++ Axa Glass/MyProtectionDetailsViewController.h
@@ -1,13 +1,13 @@
 //
-//  MyProtectionDetails.h
+//  MyProtectionDetailsViewController.h
 //  Axa Glass
 //
 //  Created by Lorenz Hänggi on 01/10/14.
 //  Copyright (c) 2014 Axa. All rights reserved.
 //
 
-#ifndef Axa_Glass_MyProtectionDetails_h
-#define Axa_Glass_MyProtectionDetails_h
+#import <UIKit/UIKit.h>
 
+@interface MyProtectionDetailsViewController : UIViewController<UIActionSheetDelegate>
 
-#endif
+@end
---
+++ Axa Glass/MyProtectionDetailsViewController.m
@@ -0,0 +1,51 @@
+//
+//  MyProtectionDetailsViewController.m
+//  Axa Glass
+//
+//  Created by Nicolas Lauquin on 11/09/2014.
+//  Copyright (c) 2014 Axa. All rights reserved.
+//
+
+#import "MyProtectionDetailsViewController.h"
+
+@interface MyProtectionDetailsViewController ()
+
+@end
+
+
+@implementation MyProtectionDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    //self.navigationItem.titleView = nil;
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Navigation
+
+
+
+
+
+
+@end
