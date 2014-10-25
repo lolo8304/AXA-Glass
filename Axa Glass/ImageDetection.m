afb44b233c95c74ad16d77677c6935dba8c2d54a
Lorenz Hänggi <lolo8304@gmail.com>
2014-10-25 16:04:36 +0200

    remove old code. needs heavy refactoring now

--- Axa Glass/ImageDetection.m
+++ Axa Glass/ImageDetection.m
@@ -100,49 +100,6 @@
     }
 }
 
-- (NSDictionary *)uploadAndDetectImage_NSMutableURL2 {
-    
-    /** based on http://nthn.me/posts/2012/objc-multipart-forms.html **/
-    
-    NSURL *url = [NSURL URLWithString:@"http://inventory42-focusdays14.rhcloud.com/UploadAndDetectServlet"];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    [request setHTTPMethod:@"POST"];
-    NSString *boundary = @"YOUR_BOUNDARY_STRING";
-    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
-    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
-
-    NSData *imageData = [NSData dataWithContentsOfURL:self.imageURL];
-    
-    NSMutableData *body = [NSMutableData data];
-    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"%@\"\r\n", self.fileName] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:imageData];
-    
-    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"submit\"\r\n\r\n%d", 1] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-
-    [request setHTTPBody:body];
-    id response = nil;
-    NSError *error = nil;
-    
-    NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-    if (error == nil && [response isKindOfClass: [NSHTTPURLResponse class]] && [response statusCode] == 200) {
-        
-        id jsonData = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:&error];
-        if (error!=nil) {
-            return nil;
-        }
-        return nil;
-    } else {
-        return nil;
-    }
-}
-
-
-
 - (NSURL *)newTempFileURLNamed: (NSString *)fileName {
     NSString* tempPath = NSTemporaryDirectory();
     NSString* tempFile = [tempPath stringByAppendingPathComponent:fileName];
